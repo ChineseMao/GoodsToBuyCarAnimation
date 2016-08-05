76a1b3a140314c387bc10ac20b689a48c2e9c3b0
毛韶谦 <2802221460@qq.com>
2016-08-05 13:29:54 +0800

    购物车 装入动画

--- goodsToBuyCarAnimation/goodsToBuyCarAnimation/BKAnimationGoods.h
+++ goodsToBuyCarAnimation/goodsToBuyCarAnimation/BKAnimationGoods.h
@@ -11,8 +11,19 @@
 
 @interface BKAnimationGoods : NSObject
 
+/**
+ *  调用 产生动画
+ *
+ *  @param clickButton 点击Button
+ *  @param fromPoint   开始的位置
+ *  @param toPoint     结束的位置
+ *  @param image       之行动画的图片
+ */
 - (void)startAnimalclickButton:(UIButton *)clickButton fromPoint:(CGPoint )fromPoint toPoint:(CGPoint )toPoint withImage:(UIImage *)image;
 
+/**
+ *  当物品放入购物车时装入动画，需先赋值；
+ */
 @property (nonatomic, strong) UIButton *buyCarButton;
 
 @end
